ccf72736fedc016ef9db33e78b22a519f202ecaf
Jason <poulain.jas@gmail.com>
2014-02-03 21:15:32 +0100

    Création du dossier

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rezo::Application.config.secret_token = '4d7865496685cf567da0fabaa3e64ee4af41dad4ec04e1543e4329ebefd46dd18ac973db47738b8df783c73bf4bcc6d7a345b59636048796dcee1c67fd426558'
